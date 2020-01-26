7701376403e4ea64351e264f4dd2e5f23a9e2ddc
David <dweisiger@gmail.com>
2020-01-25 23:06:29 -0500

    Update

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,3 +1,5 @@
+//import * as ex from './exercise.js';
+
 // Initialize Cloud Firestore through Firebase
 firebase.initializeApp({
   apiKey: 'AIzaSyDZOgyRb3XhHbxXq5oRXBlt-EyG3fpW2ww',
@@ -37,20 +39,42 @@ function logout() {
   });
 }
 
+function show_profile() {
+  clear_home();
+}
+
+function show_home() {
+  let main = document.getElementById("main");
+  main.innerHTML = '<br><h6><b>Today\'s Exercises:</b></h6><ul class="list-group"><div id="exercises"></div></ul><br><h6><b>Notes:</b></h6><textarea class="form-control" id="notes" rows="10"></textarea><br><button id="roll" type="button" class="btn btn-dark">Reroll</button>&nbsp;<button id="start_stop" type="button" class="btn btn-success">Start</button>';
+  //ex.choose_lifts();
+}
+
+function clear_home() {
+  let main = document.getElementById("main");
+  main.innerHTML = "";
+}
+
 firebase.auth().onAuthStateChanged(function (user) {
   if (user) {
     document.getElementById("displayname").innerHTML = "Hi " + user.displayName + "!";
     document.getElementById("login").innerHTML = "Logout";
     document.getElementById("login").removeEventListener("click", login);
     document.getElementById("login").addEventListener("click", logout);
+    // Set profile link
+    document.getElementById("profilelink").innerHTML = '<li class="nav-item active"><a id="profile" class="nav-link">Profile</a></li>';
+    document.getElementById("profile").addEventListener("click", show_profile);
   } else {
     document.getElementById("displayname").innerHTML = "";
     document.getElementById("login").innerHTML = "Login";
     document.getElementById("login").removeEventListener("click", logout);
     document.getElementById("login").addEventListener("click", login);
+    // Remove profile link
+    document.getElementById("profilelink").innerHTML = "";
   }
   document.getElementById("login").style = "display=visible;"; // make button visible
 });
 
+show_home();
 // Add button listeners
 document.getElementById("login").addEventListener("click", login);
+document.getElementById("home").addEventListener("click", show_home);
